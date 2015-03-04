50475e481ff1d65e80dcd0bd8b1fbe2971493cc1
Stella Miranda <stellamariam@gmail.com>
2015-03-04 15:30:45 -0500

    model validations on create update

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,11 @@ class User < ActiveRecord::Base
     before_save :encrypt_password
   
     validates_confirmation_of :password
+    validates_presence_of :name
+    validates_presence_of :celphone
     validates_presence_of :password, :on => :create
-    validates_presence_of :email
-    validates_uniqueness_of :email
+    validates_presence_of :username
+    validates_uniqueness_of :username
   
     def self.authenticate(username, password)
         user = find_by_username(username)
